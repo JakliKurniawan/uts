07ab66bc9060b010a35c7ed815818a3883e1c515
jaklikurniawan771@gmail.com <111360876+JakliKurniawan@users.noreply.github.com>
2022-11-30 21:41:21 +0800

    first commit

---
+++ resources/views/komponen.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> SKEMA </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <!-- content komponen start -->
+        <div class="komponen">KOMPONEN YANG DIPAKAI<br>1. ARDUINO UNO<br>2.  MERCURRY TILT SWITCH MODUL<br>3.  SERVO MOTOR SG-90<br>4.   LED<br>
+            5.  JOYSTICK<br>5.   ARDUINO IDE<br>6.  180 Ohm RESISTOR<br>7.  BREADBOARD<br>8.    JUMPER WIRE
+        
+        </div>    
+        <!-- content komponen end -->
+    </body>
+</html>
